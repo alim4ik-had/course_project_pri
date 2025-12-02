6182be7ce118194c061e0783800c67f74b6cf40b
alimo <alim4ik-had@yandex.ru>
2025-12-02 21:13:17 +0300

    Добавлено:  -передача объектов сервисов в модели (main.js)  -атрибут для link для отключения предупреждений (предупреждения никак не влияли на работоспособность)  -описание третьего этапа проекта

--- src/main.js
+++ src/main.js
@@ -8,9 +8,12 @@ import StatusModel from "./model/status-model.js";
 import CourseModel from "./model/course-model.js";
 import AddWindowPresenter from "./presenter/add-window-presenter.js";
 import WindowCoursePresenter from "./presenter/window-course-presenter.js";
+import StatusApiService from "./api/status-api-service.js";
+import CourseApiService from "./api/course-api-service.js";
+import {END_POINT} from "./const.js";
 
-const statusModel = new StatusModel();
-const courseModel = new CourseModel();
+const statusModel = new StatusModel(new StatusApiService(END_POINT));
+const courseModel = new CourseModel(new CourseApiService(END_POINT));
 
 const body = document.body;
 const divContainer = document.querySelector(".main-content");
@@ -20,17 +23,16 @@ const sidebar = new SidebarView();
 const courseGrid = new CoursesGridView();
 
 const sidebarPresenter = new SidebarPresenter(sidebar.element, statusModel, courseModel);
-const courseWindowPresenter = new WindowCoursePresenter(body, courseModel, sidebarPresenter);
-const courseListPresenter = new CourseListPresenter(courseGrid.element, courseModel, statusModel, sidebarPresenter, courseWindowPresenter);
-const addWindowPresenter = new AddWindowPresenter(body, courseModel, sidebarPresenter);
+const courseWindowPresenter = new WindowCoursePresenter(body, courseModel);
+const courseListPresenter = new CourseListPresenter(courseGrid.element, courseModel, statusModel, courseWindowPresenter, sidebarPresenter);
+const addWindowPresenter = new AddWindowPresenter(body, courseModel);
 
 render(new HeaderView(openAddModalWindow), body, RenderPosition.AFTERBEGIN);
 render(sidebar, divContainer, RenderPosition.AFTERBEGIN);
 
 render(courseGrid, mainContainer, RenderPosition.AFTERBEGIN);
 
-sidebarPresenter.initSidebar();
-courseListPresenter.initCourseGrid();
+await courseListPresenter.initCourseGrid();
 
 
 export function openAddModalWindow(){
